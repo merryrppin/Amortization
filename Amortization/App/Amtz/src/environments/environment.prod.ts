a026bd0f834ec110556184822c06653255f7b596
Wilmar González <merryrppin@hotmail.com>
2020-01-20 21:07:55 -0500

    Creación de la página de registro

--- Amortization/App/Amtz/src/environments/environment.prod.ts
+++ Amortization/App/Amtz/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBaIvHwHhoESGq0mZzw3G97bLcbt48NN7o",
+    authDomain: "amortization-899e1.firebaseapp.com",
+    databaseURL: "https://amortization-899e1.firebaseio.com",
+    projectId: "amortization-899e1",
+    storageBucket: "amortization-899e1.appspot.com",
+    messagingSenderId: "522176510146",
+    appId: "1:522176510146:web:e0c0b0e360a375734ee35b",
+    measurementId: "G-0CRNLCXPPR"
+  }
 };
